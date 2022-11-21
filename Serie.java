2e8286b2352f4b1b571bee1d86f3754338196f0d
unknown <aidenperse@gmail.com>
2022-11-21 11:41:23 -0500

    Series

--- Serie.java
+++ Serie.java
@@ -14,5 +14,50 @@ public class Serie {
         genero = "";
         creador = "";
     }
+    //
+    public void titulo_creador(){
+        titulo = "Peaky Blinders";
+        numero_de_temporadas = 0;
+        genero = "";
+        entregado = false;
+        creador = "Steven Knight";
+    }
+    public void no_entregado(){
+        titulo = "";
+        numero_de_temporadas = 0;
+        genero = "";
+        creador = "";
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumero_de_temporadas() {
+        return numero_de_temporadas;
+    }
+
+    public void setNumero_de_temporadas(int numero_de_temporadas) {
+        this.numero_de_temporadas = numero_de_temporadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
 }
