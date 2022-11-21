7ada898cf75a290307026d5635ff1e16b47b9fb3
unknown <aidenperse@gmail.com>
2022-11-21 11:14:24 -0500

    Inicio

---
+++ Serie.java
@@ -0,0 +1,18 @@
+public class Serie {
+    //Atributos
+    String titulo;
+    int numero_de_temporadas;
+    boolean entregado;
+    String genero;
+    String creador;
+
+    //Constructor
+    public Serie(){
+        titulo = "";
+        numero_de_temporadas = 3;
+        entregado = false;
+        genero = "";
+        creador = "";
+    }
+
+}
